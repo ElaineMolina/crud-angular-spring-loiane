40a6684a8997faacf7a1c4c8481258f630b381f3
ElaineMolina <elainemolina10@gmail.com>
2022-08-19 15:04:55 -0300

    15. Listar Cursos (API HTTP GET)

--- crud-spring/src/main/java/com/molina/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/molina/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.molina;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.molina.model.Course;
+import com.molina.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
